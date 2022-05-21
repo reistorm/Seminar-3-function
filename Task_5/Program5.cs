fd664b8debb0f6edd27961d16ecb9439047016d8
ELIZA <rysaeva96@mail.ru>
2022-05-21 13:20:01 +0500

    Решение 4 задачи

---
+++ Task_5/Program5.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("|{0,10}|{1,10}|", "Число", "Корень");
+for (int i = 1; i <= length; i++) 
+Console.WriteLine("|{0,10}|{1,10}|", i, i*i);   
+
+
+
