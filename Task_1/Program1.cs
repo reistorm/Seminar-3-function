20680eec38763ae8d3af1a7828ba36d19e94126a
ELIZA <rysaeva96@mail.ru>
2022-05-21 12:28:11 +0500

    Решение 1 задачи

---
+++ Task_1/Program1.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу при вводе значений определить
+// номер плоскости
+
+Console.WriteLine("Введите значение х: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение y: ");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0 )
+{
+    Console.WriteLine("Первая четверть");
+}
+else if (x < 0 && y > 0 )
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if (x < 0 && y < 0 )
+{
+    Console.WriteLine("Третья четверть");
+}
+else if (x > 0 && y < 0 )
+{
+    Console.WriteLine("Четвертая четверть");
+}
