6119926bc1a3f3d4e0a2aef973b5b1f2d49d2020
ELIZA <rysaeva96@mail.ru>
2022-05-21 12:32:09 +0500

    Решение 3 задачи

---
+++ Task_3/Program3.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на
+// вход координаты двух точек и находит 
+// расстояние между ними в 2D пространстве.
+
+Console.Write("Введите координату х первой точки = ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y первой точки = ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату х первой точки = ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y первой точки = ");
+int y2 = int.Parse(Console.ReadLine());
+
+int res1 = x1 - x2;
+int res2 = y1 - y2;
+
+int result1 = res1 * res1 + res2 * res2;
+double result = Math.Sqrt(result1);
+Console.WriteLine("{0: 0.000}" ,result);
+
